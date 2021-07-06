ee4ad4efc6340a739456cba21d1e723af731fd84
Manyu Lakhotia <abhimanyu.lakhotia@acadian.com>
2021-07-06 14:14:41 -0500

    Moves .editorconfig to root of project

--- Acadian.Json.Fs.Tests/Tests.fs
+++ Acadian.Json.Fs.Tests/Tests.fs
@@ -9,63 +9,49 @@ type Item = { Id: int; Desc: string option }
 
 [<Tests>]
 let tests =
-    testList
-        "Tests"
-        [ testList
-              "JsonConverters.OptionConverter"
-              [ test "Test converting a list of items with optional members to json" {
-                  let items =
-                      [ { Id = 1; Desc = None }
-                        { Id = 2; Desc = Some "stuff" } ]
+  testList
+    "Tests"
+    [ testList
+        "JsonConverters.OptionConverter"
+        [ test "Test converting a list of items with optional members to json" {
+            let items = [ { Id = 1; Desc = None }; { Id = 2; Desc = Some "stuff" } ]
 
-                  let jsons =
-                      JsonConvert.SerializeObject(items, OptionConverter())
+            let jsons = JsonConvert.SerializeObject(items, OptionConverter())
 
-                  jsons
-                  |> Expect.equal "" "[{\"Id\":1,\"Desc\":null},{\"Id\":2,\"Desc\":\"stuff\"}]"
-                }
-                test "Test converting an item with None desc json" {
-                    let item = { Id = 1; Desc = None }
+            jsons |> Expect.equal "" "[{\"Id\":1,\"Desc\":null},{\"Id\":2,\"Desc\":\"stuff\"}]"
+          }
+          test "Test converting an item with None desc json" {
+            let item = { Id = 1; Desc = None }
 
-                    let json =
-                        JsonConvert.SerializeObject(item, OptionConverter())
+            let json = JsonConvert.SerializeObject(item, OptionConverter())
 
-                    json |> Expect.equal "" "{\"Id\":1,\"Desc\":null}"
-                }
-                test "Test converting an item with Some desc json" {
-                    let item = { Id = 1; Desc = Some "stuff" }
+            json |> Expect.equal "" "{\"Id\":1,\"Desc\":null}"
+          }
+          test "Test converting an item with Some desc json" {
+            let item = { Id = 1; Desc = Some "stuff" }
 
-                    let json =
-                        JsonConvert.SerializeObject(item, OptionConverter())
+            let json = JsonConvert.SerializeObject(item, OptionConverter())
 
-                    json
-                    |> Expect.equal "" "{\"Id\":1,\"Desc\":\"stuff\"}"
-                }
-                test "Test converting an item that is None" {
-                    let item = None
+            json |> Expect.equal "" "{\"Id\":1,\"Desc\":\"stuff\"}"
+          }
+          test "Test converting an item that is None" {
+            let item = None
 
-                    let json =
-                        JsonConvert.SerializeObject(item, OptionConverter())
+            let json = JsonConvert.SerializeObject(item, OptionConverter())
 
-                    json |> Expect.equal "" "null"
-                }
-                test "Test converting an item list that is empty" {
-                    let item = []
+            json |> Expect.equal "" "null"
+          }
+          test "Test converting an item list that is empty" {
+            let item = []
 
-                    let json =
-                        JsonConvert.SerializeObject(item, OptionConverter())
+            let json = JsonConvert.SerializeObject(item, OptionConverter())
 
-                    json |> Expect.equal "" "[]"
-                }
-                test "Test converting a list of optional items with optional members to json" {
-                    let items =
-                        [ Some { Id = 1; Desc = None }
-                          Some { Id = 2; Desc = Some "stuff" }
-                          None ]
+            json |> Expect.equal "" "[]"
+          }
+          test "Test converting a list of optional items with optional members to json" {
+            let items = [ Some { Id = 1; Desc = None }; Some { Id = 2; Desc = Some "stuff" }; None ]
 
-                    let jsons =
-                        JsonConvert.SerializeObject(items, OptionConverter())
+            let jsons = JsonConvert.SerializeObject(items, OptionConverter())
 
-                    jsons
-                    |> Expect.equal "" "[{\"Id\":1,\"Desc\":null},{\"Id\":2,\"Desc\":\"stuff\"},null]"
-                } ] ]
+            jsons |> Expect.equal "" "[{\"Id\":1,\"Desc\":null},{\"Id\":2,\"Desc\":\"stuff\"},null]"
+          } ] ]
